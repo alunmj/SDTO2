2912f78bc96a278142f3160d80af84a524c192ce
Alun Jones <mcsp@texis.com>
2025-08-28 21:41:27 -0700

    Remove jquery Clean formatting Add error if DNS to google is blocked.

--- background.js
+++ background.js
@@ -1,46 +1,45 @@
-chrome.webRequest.onErrorOccurred.addListener(onErrorOccurred, {urls: ["<all_urls>"]});
-chrome.webRequest.onCompleted.addListener(onCompleted, {urls: ["<all_urls>"]});
+chrome.webRequest.onErrorOccurred.addListener(onErrorOccurred, { urls: ["<all_urls>"] });
+chrome.webRequest.onCompleted.addListener(onCompleted, { urls: ["<all_urls>"] });
 
 // Storage API details at https://developer.chrome.com/docs/extensions/reference/api/storage
 
-async function onErrorOccurred(details)
-{
+async function onErrorOccurred(details) {
   if (!navigator.onLine) {
     return; // Could just be because we disconnected.
   }
   if (details.error == "net::ERR_NAME_NOT_RESOLVED") {
     //debugger;
-    response = await fetch("https://dns.google/resolve?name="+(new URL(details.url)).host+"&type=5")
+    response = await fetch("https://dns.google/resolve?name=" + (new URL(details.url)).host + "&type=5")
+    cnameExtra = "";
+    cname = ""
     if (response.status == 200) {
-        jq = await response.json();
-        //debugger
-        if (jq.Answer) {
-          cnameExtra = "\r\n \r\nCNAME for: " + jq.Answer[0].data
-          cname = jq.Answer[0].data
-        } else {
-          cnameExtra = "";
-          cname = ""
-        }
-        datatowrite = {"desturl":details.url,"cname":cname}
-        chrome.tabs.get(details.tabId,function(tab) {
-          datatowrite.pageurl = tab.url
-          theHost = (new URL(details.url)).hostname
-          datatowrite.dest = theHost
-          chrome.storage.local.set({[theHost]:datatowrite})
-        })
-
+      jq = await response.json();
+      //debugger
+      if (jq.Answer) {
+        cnameExtra = "\r\n \r\nCNAME for: " + jq.Answer[0].data
+        cname = jq.Answer[0].data
+      }
+    } else {
+      cname = `DNS: <${response.status}>`
     }
+    datatowrite = { "desturl": details.url, "cname": cname }
+    chrome.tabs.get(details.tabId, function (tab) {
+      datatowrite.pageurl = tab.url
+      theHost = (new URL(details.url)).hostname
+      datatowrite.dest = theHost
+      chrome.storage.local.set({ [theHost]: datatowrite })
+    })
 
-//    chrome.tabs.update(details.tabId, {url: "..."});
+
+    //    chrome.tabs.update(details.tabId, {url: "..."});
   }
 }
 
-function onCompleted(details)
-{
+function onCompleted(details) {
   if (!navigator.onLine) {
     return; // Could just be because we're disconnected.
   }
   if (details.statuscode == 404) {
-    chrome.tabs.get(details.tabId,function(tab){alert('404 Source ' + tab.url + '\nDest ' + details.url)});
+    chrome.tabs.get(details.tabId, function (tab) { alert('404 Source ' + tab.url + '\nDest ' + details.url) });
   }
 }
